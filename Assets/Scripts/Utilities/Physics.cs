1f828b01f82e74213dc1b2bfebdf76e9d44efcff
SoonHyung_LEE <tnsgud9@naver.com>
2025-02-09 18:51:08 +0900

    Add CylinderCast

---
+++ Assets/Scripts/Utilities/Physics.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class Physics
+    {
+        public static RaycastHit[] CylinderCastAll(
+            Vector3 origin,
+            float radius,
+            Vector3 direction,
+            float maxDistance = Mathf.Infinity,
+            int layerMask = -5,
+            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
+        {
+            return UnityEngine.Physics
+                .SphereCastAll(origin, radius, direction, maxDistance, layerMask, queryTriggerInteraction)
+                .Where(hit => Vector3.Distance(origin, hit.point) <= maxDistance).ToArray();
+        }
+
+
+        public static bool CylinderCast(
+            Vector3 origin,
+            float radius,
+            Vector3 direction,
+            out RaycastHit hitInfo,
+            float maxDistance = Mathf.Infinity,
+            int layerMask = -5,
+            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
+        {
+            RaycastHit? hit = UnityEngine.Physics
+                .SphereCastAll(origin, radius, direction, maxDistance, layerMask, queryTriggerInteraction)
+                // .OrderBy(hit => hit.distance)
+                .FirstOrDefault(hit => Vector3.Distance(origin, hit.point) <= maxDistance);
+            if (hit != null)
+            {
+                hitInfo = (RaycastHit)hit;
+                return true;
+            }
+            hitInfo = default;
+            return false;
+        }
+
+        
+    }
+}
